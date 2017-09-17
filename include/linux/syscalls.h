526f71349749367794da6977000f71fc0814a1a5
benheberlein <ben.heberlein@gmail.com>
2017-09-16 20:49:01 -0600

    Added memsort syscall to syscall table and syscalls.h

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -906,4 +906,6 @@ asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
 
+asmlinkage long sys_memsort(int32_t __user *buf, int32_t size, int32_t *sbuf);
+
 #endif
